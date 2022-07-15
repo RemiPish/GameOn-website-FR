d15b93946167ca8e6d8910b1c1204212068b6997
Remi Phyu <remi.phyu@gmail.com>
2022-07-15 02:49:12 +0200

    added error message array

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,24 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const dateBirth = document.getElementById("datebirth");
+const quantity = document.getElementById("quantity");
+const locationList = document.querySelectorAll(
+  "checkbox-input[type=radio]:checked"
+);
+const condition = document.getElementById("checkbox1");
+
+const errorMessageArray = {
+  nameLength: "Veuillez entrer 2 caractères ou plus.",
+  emailInvalid: "Veuillez entrer une adresse mail valide.",
+  dateBirthInvalid: "Veuillez entrer une date de naissance valide.",
+  quantityInvalid: "Veuillez entrer un nombre valide.",
+  locationInvalid: "Veuillez selectionner une ville.",
+  conditionInvalid: "Veuilez accepter les conditions d'utilisation.",
+};
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
